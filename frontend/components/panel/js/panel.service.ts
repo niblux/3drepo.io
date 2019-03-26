ef4a6b7712b07a53b2555861b21ced0aa51df332
Daniel Leszczynski <dleszczynski@apptension.com>
2019-03-26 16:48:28 +0100

    ISSUE #1439 - Add Compare container with basic UI

--- frontend/components/panel/js/panel.service.ts
+++ frontend/components/panel/js/panel.service.ts
@@ -407,7 +407,8 @@ export class PanelService {
 			icon: 'compare',
 			minHeight: 265,
 			fixedHeight: false,
-			options: []
+			options: [],
+			isReactComponent: true
 		});
 
 		this.panelCards.left.push({
