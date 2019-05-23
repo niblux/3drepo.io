77aa6ca3ec359770435aa7f7a1f8772109262fd3
Mariusz Wójcik <mwojcik@apptension.com>
2019-05-23 09:18:49 +0200

    ISSUE #1475 - Performance fixes

--- frontend/modules/viewer/viewer.sagas.ts
+++ frontend/modules/viewer/viewer.sagas.ts
@@ -311,7 +311,7 @@ export function* setMeasureVisibility({ visible }) {
 
 export function* clearHighlights() {
 	try {
-		yield Viewer.clearHighlights();
+		Viewer.clearHighlights();
 	} catch (error) {
 		yield put(DialogActions.showErrorDialog('clear', 'highlights'));
 	}
